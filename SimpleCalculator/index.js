d25725b29ea2631c1b7a02e86ec8215f5c142374
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2022-01-03 14:57:22 +0200

    upload calc
    Calc wich works only with divide 'for now'!

---
+++ SimpleCalculator/index.js
@@ -0,0 +1,53 @@
+
+let hiddenInputField = '';
+let operation = '';
+let isClear = false;
+
+function press (e) {
+    let btnNumber = e.target.innerText;
+
+    if (hiddenInputField === '') {
+        let input = document.getElementById('in').value += btnNumber;
+    } else {
+        if (!isClear) {
+            document.getElementById('in').value = btnNumber;
+            isClear = true;
+        } else {
+            document.getElementById('in').value += btnNumber;
+        }
+    }
+}
+
+function dotPress (e) {
+    let text = document.getElementById('in').value;
+    if (!text.includes('.')) {
+        let dot = e.target.innerText;
+        document.getElementById('in').value += dot;
+    }
+}
+
+function divide(e) {
+    if (operation === '') {
+        hiddenInputField = document.getElementById('in').value;
+        operation = e.target.innerText;
+        
+    } else if (operation === '/' && isClear) {
+        equal();
+        hiddenInputField = document.getElementById('in').value;
+        operation = e.target.innerText;
+    }
+}
+
+function equal (e) {
+    let firstNum = Number(hiddenInputField);
+    let secondNum = Number(document.getElementById('in').value);
+    let result;
+    if(operation === '/') {
+        result = firstNum / secondNum;
+        document.getElementById('in').value = result;
+        hiddenInputField = '';
+        operation = '';
+        isClear = false;
+    }
+    
+}
